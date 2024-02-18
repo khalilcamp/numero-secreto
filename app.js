fdcb97a812bfc0336d644fa66836f0d73220a0e3
Khalil Camargo Packer <kacamargopacker@gmail.com>
2024-02-18 18:30:47 -0300

    Adicionadas novas opções de jogo.

--- app.js
+++ app.js
@@ -1,61 +1,53 @@
-//Criar uma variável para poder ser alterada. InnerHTML é uma alteração DENTRO do HTML.
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Sexo';
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha 1 numero entre 1 e 10';
-
-// tag seria utilizado para tags vazias.
 let listaNumeros = [];
-let numeroLimite = 50;
+let numeroLimite = 50; // Número padrão
 let numeroSecreto = gerarNumero();
 let tentativas = 1;
 
 function exibirTexto(tag, texto) {
-    let campo = document.querySelector(tag)
-        campo.innerHTML = texto;
-        responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2} );
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
-function exibirMensagemInicial(){
+function exibirMensagemInicial() {
     exibirTexto('h1', 'Bem vindo ao jogo do Numero Secreto');
-        exibirTexto('p', 'Escolha 1 numero entre 1 e 10');
+    exibirTexto('p', `Escolha 1 número entre 1 e ${numeroLimite}`);
 }
 
-
-// function serve como uma função. Nesse caso, verificar o clique.
-// verifica o valor e retorna se é falso ou verdadeiro.
+function escolherOpcao(opcao) {
+    numeroLimite = opcao;
+    numeroSecreto = gerarNumero();
+    exibirMensagemInicial();
+}
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
     console.log(chute == numeroSecreto);
-    if (chute == numeroSecreto){
+    if (chute == numeroSecreto) {
         exibirTexto('h1', `Acertou!`);
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa' ;
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu com ${tentativas} ${palavraTentativa}!`;
-        exibirTexto('p',  mensagemTentativas);
+        exibirTexto('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
-    }else{
-        if (chute > numeroSecreto){
+    } else {
+        if (chute > numeroSecreto) {
             exibirTexto('p', 'O número é menor');
         } else {
-            exibirTexto('p', 'O número é maior')
+            exibirTexto('p', 'O número é maior');
         }
         tentativas++;
         limparCampo();
     }
 }
 
-// função que retornaa um resultado.
-
-function gerarNumero () {
-    let numeroEscolhido = parseInt(Math.random() * numeroLimite+1);
+function gerarNumero() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeElementos = listaNumeros.length;
 
     if (quantidadeElementos == 10) {
         listaNumeros = [];
     }
-    if (listaNumeros.includes(numeroEscolhido)){
+    if (listaNumeros.includes(numeroEscolhido)) {
         return gerarNumero();
     } else {
         listaNumeros.push(numeroEscolhido);
@@ -63,12 +55,12 @@ function gerarNumero () {
     }
 }
 
-function limparCampo(){
-    chute = document.querySelector('input');
+function limparCampo() {
+    let chute = document.querySelector('input');
     chute.value = '';
 }
 
-function reiniciarJogo(){
+function reiniciarJogo() {
     numeroSecreto = gerarNumero();
     limparCampo();
     tentativas = 1;
